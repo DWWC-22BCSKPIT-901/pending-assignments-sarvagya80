8e6dc35941dac48229bdd1311d1b83949695c557
sarvagya80 <adityapathak8081@gmail.com>
2024-12-28 13:52:15 +0530

    Update A4.cpp

--- A4.cpp
+++ A4.cpp
@@ -1,35 +1,35 @@
 #include <iostream>
-#include <climits>
+#include <vector>
 using namespace std;
 
-struct TreeNode {
-    int val;
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-};
-
-int maxPathSumHelper(TreeNode* root, int& maxSum) {
-    if (!root) return 0;
-    int leftSum = max(0, maxPathSumHelper(root->left, maxSum));
-    int rightSum = max(0, maxPathSumHelper(root->right, maxSum));
-    maxSum = max(maxSum, leftSum + rightSum + root->val);
-    return root->val + max(leftSum, rightSum);
+int dfs(vector<vector<int>>& grid, int i, int j) {
+    if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == 0) return 0;
+    grid[i][j] = 0;
+    return 1 + dfs(grid, i + 1, j) + dfs(grid, i - 1, j) + dfs(grid, i, j + 1) + dfs(grid, i, j - 1);
 }
 
-int maxPathSum(TreeNode* root) {
-    int maxSum = INT_MIN;
-    maxPathSumHelper(root, maxSum);
-    return maxSum;
+int maxAreaOfIsland(vector<vector<int>>& grid) {
+    int maxArea = 0;
+    for (int i = 0; i < grid.size(); ++i) {
+        for (int j = 0; j < grid[0].size(); ++j) {
+            if (grid[i][j] == 1) {
+                maxArea = max(maxArea, dfs(grid, i, j));
+            }
+        }
+    }
+    return maxArea;
 }
 
-// Example usage
 int main() {
-    TreeNode* root = new TreeNode(1);
-    root->left = new TreeNode(2);
-    root->right = new TreeNode(3);
-
-    cout << "Maximum Path Sum: " << maxPathSum(root) << endl;
-
+    vector<vector<int>> grid = {
+        {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+        {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+        {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0}
+    };
+    cout << "Max Area of Island: " << maxAreaOfIsland(grid) << endl;
     return 0;
 }
