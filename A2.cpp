d99825a3628aad47139a00a9b630480a8959cf94
sarvagya80 <adityapathak8081@gmail.com>
2024-12-28 13:47:44 +0530

    Update A2.cpp

--- A2.cpp
+++ A2.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+bool isSameTree(TreeNode* p, TreeNode* q) {
+    if (!p && !q) return true;
+    if (!p || !q || p->val != q->val) return false;
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+}
+
+// Example usage
+int main() {
+    TreeNode* p = new TreeNode(1);
+    p->left = new TreeNode(2);
+    p->right = new TreeNode(3);
+
+    TreeNode* q = new TreeNode(1);
+    q->left = new TreeNode(2);
+    q->right = new TreeNode(3);
+
+    cout << (isSameTree(p, q) ? "true" : "false") << endl;
+
+    return 0;
+}
 
